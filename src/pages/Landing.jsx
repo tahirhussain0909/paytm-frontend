b20069819141b01d6e86755d130ed6cd89feebf4
tahirhussain0909 <tahirhussain5112@gmail.com>
2024-06-04 20:51:33 +0530

    Ready to deploy

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,26 @@
+import { useNavigate } from 'react-router-dom'
+
 export const Landing = () => {
-  return <div>
-    Landing
-  </div>
+  const navigate = useNavigate()
+
+  return (
+    <div className='flex gap-10 h-screen items-center justify-center'>
+      <button
+        onClick={() => {
+          navigate('/signup')
+        }}
+        className='border px-10 py-2 bg-slate-500 text-white hover:bg-slate-700'
+      >
+        Sign up
+      </button>
+      <button
+        onClick={() => {
+          navigate('/signin')
+        }}
+        className='border px-10 py-2 bg-blue-500 text-white hover:bg-blue-700'
+      >
+        Sign in
+      </button>
+    </div>
+  )
 }
